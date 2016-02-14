ca1f81eeef96e4b8ec236e70670dd7ad4aa25cdf
vikas-dolare17 <vikas.dolare17@gmail.com>
2016-02-14 12:14:50 +0530

    added new calss NewClassAdded.java and modify Car Interface

--- FactoryDesingPattern/src/com/fdp/beans/Car.java
+++ FactoryDesingPattern/src/com/fdp/beans/Car.java
@@ -2,9 +2,14 @@ package com.fdp.beans;
 
 public interface Car {
 		
+	// This are the abstract method which will Implement in the new Cars 
+	
 		abstract public String carName();
+		
 		abstract public String carColor();
+		
 		abstract public String carEngine();
+		
 		abstract public String carStaring();
 		
 }
---
+++ FactoryDesingPattern/src/com/fdp/beans/NewClassAdded.java
@@ -0,0 +1,11 @@
+package com.fdp.beans;
+
+public class NewClassAdded {
+	
+	public static void main(String[] args){
+		
+		System.out.println("Well come the Design Pattern Section.....!");
+		
+	}
+
+}
